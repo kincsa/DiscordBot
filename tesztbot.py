07fdd71aac5641a120f3edb75ea2a12f05675aef
Ákos Kincs <kincsakos23@gmail.com>
2020-05-10 10:01:36 +0200

    The basic Discord bot is ready to go

---
+++ tesztbot.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+with open("adatok.txt") as f:
+	auth = f.readlines()
+auth = [x.strip() for x in auth]
+
+bot = commands.Bot(command_prefix = "!")
+CHANNEL_ID = int(auth[1])
+
+@bot.command()
+async def szia(ctx):
+	await ctx.send("Szia!")
+
+bot.run(auth[0])
